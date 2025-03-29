30e180dc81f6809ca55f94ab44ef4851813e5b03
Ilya <ilyagliba02@gmail.com>
2025-03-29 19:33:24 +0200

    Done

---
+++ fabric_methhod/fabric_methhod.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Transport {
+public:
+    virtual ~Transport() {}
+    virtual std::string Deliver() const = 0;
+};
+
+class Truck : public Transport {
+public:
+    std::string Deliver() const override {
+        return "Truck deliver!";
+    }
+};
+
+class Ship : public Transport {
+public:
+    std::string Deliver() const override {
+        return "Ship deliver!";
+    }
+};
+
+class Logistic {
+public:
+    virtual ~Logistic() {};
+    virtual Transport* FactoryMethod() const = 0;
+    std::string SomeOperation() const {
+        Transport* transport = this->FactoryMethod();
+        std::string result = "Logistic: created " + transport->Deliver();
+        delete transport;
+        return result;
+    }
+};
+
+class CreateTruck : public Logistic {
+public:
+    Transport* FactoryMethod() const override {
+        return new Truck();
+    }
+};
+
+class CreateShip: public Logistic {
+public:
+    Transport* FactoryMethod() const override {
+        return new Ship();
+    }
+};
+
+int main() {
+    
+}
