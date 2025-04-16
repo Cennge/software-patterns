14f674a61bf776af1ac1e6dba0f30cd6027fb713
Ilya <ilyagliba02@gmail.com>
2025-04-16 11:04:59 +0300

    done

---
+++ ConsoleApplication5/ConsoleApplication5.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Payment {
+public:
+    virtual void pay(double amount) = 0;
+    virtual ~Payment() {}
+};
+
+class CreditCard : public Payment {
+public:
+    void pay(double amount) override {
+        cout << "Оплата " << amount << " с помощью кредитной карты.\n";
+    }
+};
+
+class Cash : public Payment {
+public:
+    void pay(double amount) override {
+        cout << "Оплата " << amount << " наличными.\n";
+    }
+};
+
+void processPayment(Payment* paymentMethod, double amount) {
+    paymentMethod->pay(amount);
+}
+
+int main() {
+    Payment* payment1 = new CreditCard();
+    Payment* payment2 = new Cash();
+
+    processPayment(payment1, 1500.50);
+    processPayment(payment2, 780.20);
+
+    delete payment1;
+    delete payment2;
+
+    return 0;
+}
